b360f2913f26c443ccf56979cf56466991b8f4d4
stophhc <stophhc@163.com>
2019-07-05 18:36:57 +0800

    1

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/*
+import './css/style.css'
+document.write("ddd");*/
+const path = require('path');
+const Vue = require('vue');
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!'
+    }
+});
