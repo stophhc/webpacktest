94934c83fa5af015a301a593027d4f882ec410a7
chendongh <chendongh@szgla.cn>
2019-08-01 18:29:23 +0800

    1

--- src/components/focus.vue
+++ src/components/focus.vue
@@ -1,13 +1,52 @@
 <template>
-    
+    <div class="focus">
+        <div class="tab">
+            <div class="hhc">
+                <li class="list1"><span class="tabIn">官方</span></li>
+                <li class="list2"><span>媒体</span></li>
+            </div>
+            <div class="swf focus_hover" id="swf_shop">
+                <div class="item">
+                    <ul>
+                    </ul>
+                </div>
+            </div>
+            <div class="swf" id="swf_shop1">
+                <div class="item1">
+                    <ul>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    //import 'https://lw2.ssl.q1.com/news/js/cache/ablumn_16.js'
     export default {
         name: "focus"
     }
+    $.getJSON("https://lw2.ssl.q1.com/news/js/cache/ablumn_17.js?jsoncallback=?",
+        function(){
+        alert('ddd');
+    });
 </script>
 
 <style scoped>
-
+    .focus{position: absolute; top: 0;left: 0;}
+    .fucus1{width: 610px;height: 275px;float: left;}
+    .tab{width:610px;height:275px; position:relative;}
+    .swf{width:610px;height:275px;position:relative; display:none;}
+    .swf img{position:absolute;left:0px;top:0px;width:610px;height:275px;}
+    .item ,.item1{height:25px;position:absolute;bottom:0px;right:20px; z-index:1}
+    .item ul li ,.item1 ul li{width:12px;height:12px;float:left;text-align:center;margin-left:5px;display:inline;line-height:25px;cursor:pointer;overflow:hidden; font-size:12px; color:#abd4ee; text-indent:-9999px; background:url(https://img.ssl.q1.com/lw/work/images/focus/dian.png) no-repeat;}
+    .item ul li.hover ,.item1 ul li.hover{background:url(https://img.ssl.q1.com/lw/work/images/focus/dian_hover.png?v=1) no-repeat;}
+    .hhc{ background:#45427e;width:23px; height:91px; position:absolute; right:0; top:0; z-index:121}
+    .hhc li{height:48px; list-style:none; cursor:pointer; display:inline;  font-size:12px; color:#fff; text-align:center;}
+    .hhc li span{ width:23px;display:block;margin:0; padding:0;}
+    .hhc li.list1 span{ height:51px; padding-top:5px; text-align:center;line-height: 15px;}
+    .hhc li.list2 span{height:38px; margin-top:-18px; padding-top:15px;text-align:center;line-height: 15px;}
+    .hhc li.list1 span.tabIn{background:url(https://img.ssl.q1.com/lw/work/images/focus/tab3_hover.gif?v=1) no-repeat;}
+    .hhc li.list2 span.tabIn{background:url(https://img.ssl.q1.com/lw/work/images/focus/tab2_hover.gif?v=1) no-repeat;}
+    .focus_hover{ display:block;}
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -4,7 +4,33 @@
         <div class="main">
             <app-left></app-left>
             <div class="right">
-
+                <app-focus></app-focus>
+                <!--新闻-->
+                <div class="new loop">
+                    <div class="title">
+                        <ul>
+                            <li class="tit_hover"><a href="/JzNewsFile/JZT0/P1.html" target="_blank">综合</a></li>
+                            <li><a href="#" target="_blank">公告</a></li>
+                            <li><a href="#" target="_blank">活动</a></li>
+                            <li><a href="#" target="_blank">新闻</a></li>
+                            <li><a href="#" target="_blank">维护</a></li>
+                        </ul>
+                        <a href="/JzNewsFile/JZT0/P1.html" target="_blank"></a> </div>
+                    <div class="p1">
+                        <div class="p1N">
+                            <div class="p1_list hui">
+                                <ul>
+                                    <li><span class=date>[07/31]</span><span class=typename>【<a href='#' target=_blank>公告</a>】</span><a href='#' target='_blank'><b><font color=red>8月2日14时1组新服火爆开启..</font></b></a>&nbsp;<span></span> </li>
+                                    <li><span class=date>[07/30]</span><span class=typename>【<a href='#' target=_blank>维护</a>】</span><a href='#' target='_blank'>7月31日7:00~9:00更新维护公..</a>&nbsp;<span></span> </li>
+                                </ul>
+                            </div>
+                            <div class="p1_list"></div>
+                            <div class="p1_list"></div>
+                            <div class="p1_list"></div>
+                            <div class="p1_list"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -19,10 +45,12 @@
 <script>
     import Top from '@/components/top'
     import Left from '@/components/left'
+    import Focus from '@/components/focus'
     export default {
         components:{
             appTop:Top,
-            appLeft:Left
+            appLeft:Left,
+            appFocus:Focus,
         }
     }
 </script>
@@ -30,4 +58,27 @@
 <style scoped>
     .main{width: 1200px;margin: 0 auto}
     .right{position: relative;width: 948px; float:right;}
+    .new{width: 293px;height: 260px;background:#fff; float: right;padding:15px 15px 0 15px;}
+    .new:hover ,.ziliao:hover ,.zhiye:hover ,.wjzs:hover ,.wjzq:hover ,.media:hover ,.fast:hover ,.service:hover ,.left_btm:hover ,.marvellous:hover ,.main_fd:hover{box-shadow:0 0 10px rgba(0,0,0,.3)}
+    .title li{ float: left;cursor: pointer; font-size: 14px; line-height: 30px; width: 58px; text-align: center;}
+    .title{ overflow: hidden;zoom:1;position: relative;}
+    .title ul{border-bottom: 1px solid #b2b2b2; height: 30px;}
+    .bor{ background:#7b7cd8;width: 55px; height: 3px; position: absolute; left: 0; top:28px; z-index: 1}
+    .tit_hover{ color: #7778d7; }
+    .p1_list li{font-size: 12px; color: #8f8f8f; line-height: 33px;}
+    .p1_list li a{color: #8f8f8f}
+    .date{float: right;}
+    .p1{position: relative; overflow: hidden;width: 293px; height: 230px;}
+    .p1_list{float: left;width: 293px; display:none;}
+    .hui{ display:block}
+    .p1N{position: absolute;}
+    .p1_list li.first_child{ color: #ed8578;font-size: 15px; text-align: center; padding: 16px 0 7px 0;font-weight: bold;}
+    .p1_list li.first_child a{color: #ff0000;}
+    .first_child .date{ display: none; }
+    .first_child .typename{ display: none; }
+    .new .p1N{}
+    .new .tit_hover{ border-bottom:2px solid #7b7cd8; font-weight:bold;}
+    .new .tit_hover a{color:#de0000}
+    .new .title a{ background:none;}
+    .new a:hover{ color:#de0000}
 </style>
