b5899e0d17a4b451f4687f778020716ed6ef8594
chendongh <chendongh@szgla.cn>
2019-09-04 18:13:31 +0800

    1

--- src/views/VueParameter/WholeAPI.vue
+++ src/views/VueParameter/WholeAPI.vue
@@ -1,9 +1,23 @@
 <template>
 <div>
   <div class="VueList">
-    <h2>nextTick</h2>
+    <h2>Vue.filter</h2>
+    <p>过滤器</p>
+    <div>{{'2019'|filterA|filterB}}</div>
+  </div>
+
+  <div class="VueList">
+    <h2>Vue.directive</h2>
+    <p>自定义指令</p>
+    <div v-hello="color3">{{message}}</div>
+  </div>
+
+  <div class="VueList">
+    <h2>Vue.nextTick</h2>
+    <p>实时更新</p>
     <div ref="nextTickDiv">{{oNextTick.oNextTick1}}</div>
     <div>{{oNextTick.oNextTick2}}</div>
+    <div>{{oNextTick.oNextTick3}}</div>
     <button @click="changeNextTick">改变</button>
   </div>
 
@@ -13,16 +27,18 @@
   </div>
 
   <div class="VueList">
-    <h2>set</h2>
+    <h2>set、delete</h2>
     姓名：{{ items.nameId }}<br>
     年龄：{{items.age}}<br>
     说明：{{items.info}}<br>
     <button @click="changeSet">点击改变</button>
   </div>
+
 </div>
 </template>
 
 <script>
+// extend
 import Vue from 'vue'
 var Profile = Vue.extend({
   template: '<p>{{firstName}}</p>',
@@ -33,6 +49,17 @@ var Profile = Vue.extend({
   }
 })
 
+// 自定义指令
+Vue.directive('hello', {
+  bind: function (el, binding, vnode) {
+    el.innerHTML = 'name:' + binding.name + '<br>' +
+      'value:' + binding.value + '<br>' +
+      'value:' + binding.expression + '<br>'
+  },
+  unbind: function (el, binding, vnode) {
+    console.log('dd')
+  }
+})
 export default {
   name: 'WholeAPI',
   data () {
@@ -46,16 +73,33 @@ export default {
         oNextTick1: '11',
         oNextTick2: '22',
         oNextTick3: '33'
-      }
+      },
+      message: 10,
+      color3: 'red'
+    }
+  },
+  filters: {// 过滤器
+    filterA: function (value) {
+      return value + '年'
+    },
+    filterB: function (value) {
+      return value + '46464646'
     }
   },
   methods: {
     changeSet () {
       // Vue.set( target, key, value ) target(改变数据的来源)，key(要更改的具体数据)，value(重新赋的值)
       this.$set(this.items, 'age', '22')
+
+      // vue.$delete(数据的来源,删除的数据)
+      this.$delete(this.items, 'info')
     },
     changeNextTick () {
+      this.oNextTick.oNextTick1 = 'Hello world'
       this.oNextTick.oNextTick2 = this.$refs.nextTickDiv.innerHTML
+      this.$nextTick(() => {
+        this.oNextTick.oNextTick3 = this.$refs.nextTickDiv.innerHTML
+      })
     }
   },
   mounted () {
@@ -67,5 +111,6 @@ export default {
 
 <style scoped>
   .VueList{width: 1000px;margin: 100px auto;}
-  h2{margin-bottom: 20px;font-size: 20px;}
+  h2{margin-bottom: 10px;font-size: 20px;}
+  p{font-size: 14px;padding-bottom: 10px;}
 </style>
